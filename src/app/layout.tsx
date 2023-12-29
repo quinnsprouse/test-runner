131e018a40b7ab916c3664b80e88573ff4003ad0
qsprouse <qsprouse@medinformatix.com>
2023-12-29 12:15:30 -0700

    update home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full bg-white">
       <ClerkProvider>
-        <body className={inter.className}>{children}</body>
+        <body className={`${inter.className} h-full`}>{children}</body>
       </ClerkProvider>
     </html>
   );
